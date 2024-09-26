b8f0fcf22f0eed9bec7d652d930016059a68c97d
TrBinhMinh <bminh9900@gmail.com>
2024-09-26 07:29:36 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'yoyo',
+  webDir: 'dist'
+};
+
+export default config;
